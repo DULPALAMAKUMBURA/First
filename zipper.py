e61a241ff1cdb3f7b3f67abf03423d282c1ed851
DULPALAMAKUMBURA <36435884+DULPALAMAKUMBURA@users.noreply.github.com>
2023-01-21 03:37:10 +0530

    Update zipper.py

--- zipper.py
+++ zipper.py
@@ -1,8 +1,8 @@
 import os
 import shutil
 
-folder_to_zip = '/path/to/folder'
-zip_file_name = '/path/to/zip/file.zip'
+folder_to_zip = '/home/torrent'
+zip_file_name = 'torrent.zip'
 
 shutil.make_archive(zip_file_name, 'zip', folder_to_zip)
 
