5fccc8299f5784d2e9b5f804f9ff3cf8480d049f
DULPALAMAKUMBURA <36435884+DULPALAMAKUMBURA@users.noreply.github.com>
2023-01-21 04:00:33 +0530

    Update python.py

--- python.py
+++ python.py
@@ -55,3 +55,15 @@ end = time.time()
 print(handle.name(), "COMPLETE")
 print("Elapsed Time: ", int((end - begin) // 60), "min :", int((end - begin) % 60), "sec")
 print(datetime.datetime.now())
+
+#zipping
+
+folder_to_zip = '/home/Torrent'
+zip_file_name = 'torrent.zip'
+
+shutil.make_archive(zip_file_name, 'zip', folder_to_zip)
+
+# Delete the folder and its contents
+shutil.rmtree(folder_to_zip)
+print(f"{zip_file_name} created successfully.")
+
