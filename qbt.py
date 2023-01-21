86d586532c9e5dba5986d9fa6c022ebf7199fa8a
DULPALAMAKUMBURA <36435884+DULPALAMAKUMBURA@users.noreply.github.com>
2023-01-21 06:04:23 -0700

    Create qbt.py

---
+++ qbt.py
@@ -0,0 +1,7 @@
+import subprocess
+
+# Ask for magnet link
+magnet_link = input("Enter magnet link: ")
+
+# Download torrent using qbittorrent-nox
+subprocess.run(["qbittorrent-nox", "-d", magnet_link])
