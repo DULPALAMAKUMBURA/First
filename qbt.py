d073d13cbe5b98a11d43883e71d09a930e9152f9
DULPALAMAKUMBURA <36435884+DULPALAMAKUMBURA@users.noreply.github.com>
2023-01-21 06:25:57 -0700

    Update qbt.py

--- qbt.py
+++ qbt.py
@@ -3,5 +3,14 @@ import subprocess
 # Ask for magnet link
 magnet_link = input("Enter magnet link: ")
 
+# Ask for save path
+save_path = input("Enter the path to save the torrent file: ")
+
 # Download torrent using qbittorrent-nox
-subprocess.run(["qbittorrent-nox", "-d", magnet_link])
+subprocess.run(["qbittorrent-nox", "-d", magnet_link, "--save-path=", save_path])
+
+# Wait for download to complete
+subprocess.run(["qbittorrent-nox", "-q"])
+
+# Print success message
+print("Torrent downloaded successfully")
