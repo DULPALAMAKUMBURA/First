cad6a7c30377d9e1359a61274bfc461743ab5563
DULPALAMAKUMBURA <36435884+DULPALAMAKUMBURA@users.noreply.github.com>
2023-01-28 22:49:01 +0530

    Update zipper.py

--- zipper.py
+++ zipper.py
@@ -2,7 +2,7 @@ import zipfile
 import shutil
 
 print("Starting to zip downloaded torrent file pls wait...")
-folder_to_zip = input("Enter the folder path you want to zip")
-zip_file_name = input("Enter the name of the zip file")
+folder_to_zip = input("Enter the folder path you want to zip = ")
+zip_file_name = input("Enter the name of the zip file = ")
 
 shutil.make_archive(zip_file_name, 'zip', folder_to_zip)
